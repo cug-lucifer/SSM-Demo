c6fee5b0f8022ea86ce5d03e13ed695ecca1970d
zhujingyu <1776253490@qq.com>
2024-05-06 16:15:49 +0800

    事务管理

--- src/main/java/com/itheima/config/SpringMvcConfig.java
+++ src/main/java/com/itheima/config/SpringMvcConfig.java
@@ -2,10 +2,12 @@ package com.itheima.config;
 
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 @Configuration
 @ComponentScan("com.itheima.controller")
 @EnableWebMvc
+@EnableTransactionManagement
 public class SpringMvcConfig {
 }
