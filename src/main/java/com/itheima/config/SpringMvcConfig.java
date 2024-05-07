c6d7ed2d3638aa3533f92c646e34339b8945fda5
zhujingyu <1776253490@qq.com>
2024-05-07 19:47:17 +0800

    拦截器

--- src/main/java/com/itheima/config/SpringMvcConfig.java
+++ src/main/java/com/itheima/config/SpringMvcConfig.java
@@ -1,13 +1,17 @@
 package com.itheima.config;
 
+import com.itheima.controller.interceptor.ProjectInterceptor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
 
 @Configuration
-@ComponentScan("com.itheima.controller")
+@ComponentScan({"com.itheima.controller","com.itheima.config"})
 @EnableWebMvc
 @EnableTransactionManagement
-public class SpringMvcConfig {
+public class SpringMvcConfig{
 }
---
+++ src/main/java/com/itheima/config/SpringMvcSupport.java
@@ -0,0 +1,16 @@
+package com.itheima.config;
+
+import com.itheima.controller.interceptor.ProjectInterceptor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
+
+public class SpringMvcSupport extends WebMvcConfigurationSupport {
+    @Autowired
+    private ProjectInterceptor projectInterceptor;
+
+    @Override
+    protected void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(projectInterceptor).addPathPatterns("/books");
+    }
+}
